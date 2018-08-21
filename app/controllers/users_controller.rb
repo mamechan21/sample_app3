8461e5b657e5725800cb9a4b75a04885cf53b58c
EC2 Default User <ec2-user@ip-172-31-72-209.ec2.internal>
2018-08-21 08:18:39 +0000

    Finish user follow/unfollow actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,8 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
+    # params[:page]はviewのwill_paginateで生成される
     @users = User.paginate(page: params[:page])
-  end
-  
-  def show
-    @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def destroy
@@ -17,9 +13,27 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   
-  private
-    
-  def admin_user
-      redirect_to(root_path) unless current_user.admin?
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+    
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
+  private
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-  has_many :followed_users, through: relationships, source: :followed
+  has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id",
                                    class_name:  "Relationship",
                                    dependent:   :destroy
@@ -21,7 +21,7 @@ class User < ApplicationRecord
   end
   
   def follow!(other_user)
-    relationships.find_by(followed_id: other_user.id)
+    relationships.create!(followed_id: other_user.id)
   end
   
   def unfollow!(other_user)
